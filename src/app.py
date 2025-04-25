a9b60a1a4e9da9f21bd0b24050676157d5aa3a40
MaTi <matiasnicolaaas@gmail.com>
2025-04-25 01:21:30 +0000

    fixed

--- src/app.py
+++ src/app.py
@@ -30,13 +30,66 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    response_body = members
 
 
     return jsonify(response_body), 200
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body =request.json
+    member = {}
+    if "id" in body:
+        member["id"] = body["id"]
+    else:
+        member["id"] = jackson_family._generateId()
+    member = {
+        **member,
+        "first_name": body["first_name"],
+        "age": body["age"],
+        "lucky_numbers": body["lucky_numbers"]
+    }
+
+    new_member = jackson_family.add_member(member)
+    response_body = {
+        "new": new_member
+    }
+
+    return jsonify(response_body), 200
+
+
+
+@app.route('/member/<int:id>', methods=['GET'])
+def member_for_id(id):
+    member = jackson_family.get_member(id)
+
+    if member:
+        formatted_member = {
+            "id": member["id"],
+            "first_name": member["first_name"], 
+            "age": member["age"],
+            "lucky_numbers": member["lucky_numbers"]
+        }
+        return jsonify(formatted_member), 200
+    else:
+        return jsonify({"msg": "Miembro no encontrado"}), 404
+    
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+
+    
+    deleted = jackson_family.delete_member(id)
+
+    if deleted:
+        return jsonify({"done": True}), 200
+    else:
+        return jsonify({"done": False}), 404
+
+    # this is how you can use the Family datastructure by calling its methods
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
